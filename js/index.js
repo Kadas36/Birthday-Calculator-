db9f51908a7053d012669f6a40151d826fa232c3
kadas36 <oduorthomas14@gmail.com>
2020-10-04 12:59:35 +0300

    Split the date

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 let doc = document.getElementById('formSubmit').addEventListener('submit', dayCalculator);
 console.log(doc);
 
+
+
 function dayCalculator(e){
     e.preventDefault();
+    let userBirthDate = document.getElementById('birthDate').value.split('-')
+    console.log(userBirthDate);
+    
 }
