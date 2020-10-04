418d93ee95f04d624aa64a84b621858d62a522fa
kadas36 <oduorthomas14@gmail.com>
2020-10-04 13:33:10 +0300

    Change strings to integers

--- js/index.js
+++ js/index.js
@@ -5,7 +5,12 @@ console.log(doc);
 
 function dayCalculator(e){
     e.preventDefault();
-    let userBirthDate = document.getElementById('birthDate').value.split('-')
-    console.log(userBirthDate);
+    let userBirthDate = document.getElementById('birthDate').value.split('-');
+    let MM = parseInt(userBirthDate[1]);
+    let DD = parseInt(userBirthDate[2]);
+    let CCYY = userBirthDate[0].split('');
+    let CC = parseInt(CCYY[0].concat(CCYY[1]));
+    let YY = parseInt(CCYY[2].concat(CCYY[3]));
+    console.log();
     
-}
+}  
